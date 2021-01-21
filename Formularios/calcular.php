da1895e89efa21ef1419d03c02f70ed65c1c11f9
Carolina <ana.carolina.mrtz.mz@gmail.com>
2021-01-20 22:04:54 -0600

    Actualizacon de Calcular

--- Formularios/calcular.php
+++ Formularios/calcular.php
@@ -5,13 +5,15 @@
 			<body style="background-color:#F397F4;">
  <h1>CALCULO DE AREAS Y PERIMETROS</h1>
 <?php
+
+
 echo "
 <form action=calcular.php method=post>
 <br>
 <tr>
 <td><h1> Elige un calculo al que desees realizar:<h1> <br></td>
 <td><h2><a href=areas.php>Calcular Areas</a></h2></td>
-
+aaaaaaaaaa 
 <td><h2><a href=perimetros.php>Calcular perimetros</a></h2></td>
 </form>
 "; 
